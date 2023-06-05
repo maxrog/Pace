99351a0c9568fedf3fe29ec6648a8f88cccc1b69
Max Rogers <>
2023-06-05 15:04:32 -0400

    add Xcode proj

---
+++ Pace/Pace/PaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaceApp.swift
+//  Pace
+//
+//  Created by Max Rogers on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct PaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
